a381a3e332ce6ebc5fd4a0ded783883966789a9c
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-18 09:33:47 +0200

    yu'

--- index.php
+++ index.php
@@ -118,23 +118,7 @@ exit();
 
             <b class="copyright">&copy; 2021 Complaint Management System </b> All rights reserved.
         </div>
-         <div style="margin-left:250px;">
-         <div class="aligncenter">
-     <script type="text/javascript">
-        amzn_assoc_ad_type = "banner";
-    amzn_assoc_marketplace = "amazon";
-    amzn_assoc_region = "US";
-    amzn_assoc_placement = "assoc_banner_placement_default";
-    amzn_assoc_campaigns = "amzn_music_bounty";
-    amzn_assoc_banner_type = "category";
-    amzn_assoc_isresponsive = "true";
-    amzn_assoc_banner_id = "1GTA90RTC5R9V7HJA902";
-    amzn_assoc_tracking_id = "fashion120eb-20";
-    amzn_assoc_linkid = "c08e19baee7fb40a6f2aef3702ff26cf";
-     </script>
-     <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
-    </div>
-    </div>
+         
     </div>
     <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
     <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
