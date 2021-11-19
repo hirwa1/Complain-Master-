61b27f394953af7a16a0a475abab94b8fc4cd047
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2021-11-19 10:51:35 +0200

    00

--- admin/chat2.php
+++ admin/chat2.php
@@ -12,7 +12,14 @@ $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 
 
+if(isset($_POST['submit']))
+{
+	$category=$_POST['category'];
+	$description=$_POST['description'];
+$sql=mysqli_query($con,"insert into category(categoryName,categoryDescription) values('$category','$description')");
+$_SESSION['msg']="Category Created !!";
 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
